622cc2efa2734806e46d590a483f615f54fdf656
William Asante <asantewilliam30@gmail.com>
2024-05-12 22:25:55 +0000

    Reverted back to promise instead of asyn await

--- script.js
+++ script.js
@@ -5,15 +5,13 @@ const btn = document.querySelector(".btn");
 const url =
   "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit";
 
-let getJoke = async () => {
-  try {
-    const response = await fetch(url);
-    const data = await response.json();
-    joke.textContent = `${data.joke}`;
-  } catch (error) {
-    console.error("Error fetching joke:", error);
-  }
+let getjoke = () => {
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      joke.textContent = `${data.joke}`;
+    });
 };
 
-btn.addEventListener("click", getJoke);
-getJoke(); // Call the function once to fetch a joke when the page loads
+btn.addEventListener("click", getjoke);
+getjoke()
